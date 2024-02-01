dbefa0963e1c7e8250ab5bd0d7f8b517e264ae7a
SalmaYasserMoselhi <148722989+SalmaYasserMoselhi@users.noreply.github.com>
2024-02-02 00:51:46 +0200

    Update ConnectionProvider.java

--- src/project/ConnectionProvider.java
+++ src/project/ConnectionProvider.java
@@ -16,7 +16,7 @@ public class ConnectionProvider {
     {
         try
         {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel","root","");  //create connection
             return con;
         }
