3bbc81746a4ae790b5b5037db06fd9d1095dcfce
SalmaYasserMoselhi <148722989+SalmaYasserMoselhi@users.noreply.github.com>
2024-02-02 03:04:20 +0200

    Add files via upload

---
+++ src/project/Select.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Saloma
+ */
+public class Select {
+    public static ResultSet getData(String query)
+    {
+        Connection con=null;
+        Statement st=null;
+        ResultSet rs=null;
+        try
+        {
+            con=ConnectionProvider.getCon();
+            st=con.createStatement();
+            rs=st.executeQuery(query);
+            return rs;
+        }
+        catch(SQLException e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
+
+
